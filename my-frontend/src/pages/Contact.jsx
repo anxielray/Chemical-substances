4184d6226137e6800bc204351032a6db6ebba360
anxielray <anxielworld@gmail.com>
2025-08-16 07:42:09 -0400

    feat(add): Add the component of the contact page

---
+++ my-frontend/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import '../index.css';
+
+export default function Contact() {
+  return (
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <p>Email: <a href="mailto:anxielworld@gmail.com">info@physiamathia.com</a></p>
+    </div>
+  );
+}
