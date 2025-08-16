8c780eb74b5761101dfc68c0f61689e02b6e7f8c
anxielray <anxielworld@gmail.com>
2025-08-16 07:41:54 -0400

    feat(add): Add the component of the about page to the frontend

---
+++ my-frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import '../index.css';
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <h1>About</h1>
+      <p>
+        This app provides information about different substances in a simple interface.
+      </p>
+    </div>
+  );
+}
